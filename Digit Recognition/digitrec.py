b6e550f37b11e46c7b7106020f5dcb54ba057916
HUGHBRADY <G00338260@gmit.ie>
2018-11-30 18:10:29 +0000

    Dataset loaded

--- Digit Recognition/digitrec.py
+++ Digit Recognition/digitrec.py
@@ -12,8 +12,12 @@ with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
 with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:  
     raw_train_lbl = f.read()
 
-# Next we'll reshape the contents and trim the excess bytes at the start
+# Next we'll trim the excess bytes at the start and convert the bytes to integers
 test_img = np.frombuffer(raw_test_img, dtype = np.uint8, offset = 16)
 test_lbl = np.frombuffer(raw_test_lbl, dtype = np.uint8, offset = 8)
 train_img = np.frombuffer(raw_train_img, dtype = np.uint8, offset = 16)
 train_lbl = np.frombuffer(raw_train_lbl, dtype = np.uint8, offset = 8)
+
+# Finally we reshape the image arrays
+test_img = test_img.reshape(10000, 28, 28)
+train_img = train_img.reshape(60000, 28, 28)
